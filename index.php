5778be1834d447d879975f516aedf6f9960d8598
TheGuyWithTheFace <thescratcherman@gmail.com>
2015-02-24 00:11:32 -0500

    added basic upload functionality

--- index.php
+++ index.php
@@ -3,4 +3,12 @@
 	<head>
 		<title>Hello World!</title>
 	</head>
+	<body>
+		<h1>Upload a file here:</h1>
+		<form enctype="multipart/form-data" action="upload.php" method="POST">
+			<input name="uploadedFile" type="file" />
+			<br />
+			<input type="submit" value="Upload" />
+		</form>
+	</body>
 </html>
---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+// location to save file
+$folder = "uploads/";
+
+// name of the file in temporary storage
+$tempName = $_FILES['uploadedFile']['tmp_name'];
+
+// actual filename
+$filename = basename( $_FILES['uploadedFile']['name'] );
+
+// move file from temp storage to permanent location in $folder
+$path = $folder . $filename;
+
+if(move_uploaded_file($tempName, $path)) {
+	echo "$filename uploaded!";
+}
+else {
+	echo "Upload failed!";
+}
+echo "<br /><a href='./index.php'>Go Back</a>";
