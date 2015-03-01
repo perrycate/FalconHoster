7f114368ac2b0825ec2447965491e5dcfdd9338c
kevinrabinovich <githubcommits@kevinrabinovich.me>
2015-02-28 20:58:42 -0500

    Replaced <head> content with head.php PHP include.

--- head.php
+++ head.php
@@ -1,4 +1,4 @@
 <link rel="icon" href="lib/favicon.png">
 <link rel="stylesheet" href="lib/styles.css">
-<title>FalconHoster<?php <title>FalconHoster<?php 
+<title>FalconHoster<?php 
     if (isset($page_title)){echo " | " . $page_title;}?></title>
